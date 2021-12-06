654e694ea78ab606df1c8396cf5723bf54d5f114
Romanna Yasinska <r.bren.1991@gmail.com>
2021-12-06 17:16:40 +0200

    task-09

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const form = document.querySelector(".login-form");
+// const formBtnEl = document.querySelector("")
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    
+    const fromElements = event.currentTarget.elements;
+    const email = fromElements.email.value;
+    const password = fromElements.password.value;
+    const formData = {
+        email , password
+    }
+
+    
+    if (email === "" || password === "") {
+        alert ("Поля не заповнені")
+    }
+    else {
+        console.log(formData);
+        form.reset();
+    }
+    
+} 
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnEl = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const textColorEl = document.querySelector(".color");
+
+
+btnEl.addEventListener("click", onBtnElClick);
+
+function onBtnElClick() {
+  body.style.backgroundColor = `${getRandomHexColor()}`
+  textColorEl.textContent=`${getRandomHexColor()}`
+}
