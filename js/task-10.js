105a1642a1a38a1ed5db978129bebd585d902c30
Romanna Yasinska <r.bren.1991@gmail.com>
2022-06-04 12:11:55 +0300

    upd

--- js/task-10.js
+++ js/task-10.js
@@ -23,31 +23,30 @@ function onButtonClick ( e ) {
   }
 }
 
-
 function createBoxes(amount) {
-  let i;
   let width = 30;
   let height = 30;
   let step = 10;
   const boxElements = [];
 
-  for (i = 1; i <= amount; i += 1) {
-    const boxEl = document.createElement('div');
-    boxEl.classList.add('box')
-    boxEl.style.width = `${width+(i-1)*step}px`
-    boxEl.style.height = `${height+(i-1)*step}px`
-    boxEl.style.backgroundColor = `${getRandomHexColor()}`
-    boxElements.push(boxEl)
+  for (let i = 1; i <= amount; i += 1) {
+    const boxEl = (width, height, i, step)=> {
+    return `<div class = "box" 
+    style="background-color:${getRandomHexColor()}; width:${width + (i - 1) * step}px; height:${height + (i - 1) * step}px">
+    </div>`;
   }
-  refs.boxesEl.append(...boxElements);
+    boxElements.push(boxEl(width, height, i, step))
+  }
+
+  refs.boxesEl.insertAdjacentHTML('afterbegin', boxElements.join(''));
   console.log(`Create ${amount} boxes`)
-  
 }
+
 
 function destroyBoxes() {
   const boxEl = document.querySelectorAll('.box');
     boxEl.forEach(element => {
-    element.remove()
+      element.remove();
     });
   console.log(`Destroy ${boxEl.length} boxes`)
 };
